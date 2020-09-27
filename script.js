6c3193dffda76e01fd44291d9da7d42a5ad27c04
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2020-09-27 14:37:28 -0400

    load all  questions

--- script.js
+++ script.js
@@ -45,6 +45,8 @@ const a_text = document.getElementById('a_text');
 const b_text = document.getElementById('b_text');
 const c_text = document.getElementById('c_text');
 const d_text = document.getElementById('d_text');
+const submitBtn = document.getElementById('submit');
+
 let currentQuiz = 0;
 
 loadQuiz();
@@ -59,6 +61,15 @@ b_text.innerText = currentQuizData.b;
 c_text.innerText = currentQuizData.c;
 d_text.innerText = currentQuizData.d;
 
-currentQuiz++;
+
 
 }
+submitBtn.addEventListener("click" , () => {
+    currentQuiz++;
+    if(currentQuiz < quizData.length){
+       
+        loadQuiz();
+    }else{
+    alert("You finished your quiz");
+    }
+});
