1ad065d3250e9c510baeeb77357471c1a12c7fcf
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2020-09-27 11:31:08 -0400

    create quiz data

---
+++ script.js
@@ -0,0 +1,42 @@
+const quizData =[
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        a: '<script>',
+        b: '<scripting>',
+        c: '<javascript>',
+        d: '<js>',
+        correct: 'a'
+    },
+    {
+        question: "How do you create a function in JavaScript?",
+        a: 'function = myFunction()',
+        b: 'function myFunction()',
+        c: 'function:myFunction()',
+        d: 'all of above',
+        correct: 'b'
+    },
+    {
+        question: "How do you call a function named myFunction?",
+        a: 'call function myFunction()',
+        b: 'call myFunction()',
+        c: 'myFunction()',
+        d: 'call:myFunction',
+        correct: 'c'
+    },
+    {
+        question: "How do you round the number 7.25, to the nearest integer?",
+        a: 'Math.round(7.25)',
+        b: 'rnd(7.25)',
+        c: 'Math.rnd(7.25)',
+        d: 'round(7.25)',
+        correct: 'a'
+    },
+    {
+        question: "How can you detect the client's browser name?",
+        a: 'client.navName',
+        b: 'browser.name',
+        c: 'navigator.appName',
+        d: 'all of above',
+        correct: 'c'
+    }
+]
