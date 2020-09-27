ecf8ffec21dbd8f51f93dbcf8ba107b613e6f2f6
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2020-09-27 15:59:50 -0400

    checking correct answer

--- script.js
+++ script.js
@@ -40,6 +40,8 @@ const quizData =[
         correct: 'c'
     }
 ]
+const  answerEls = document.querySelectorAll('.answer');
+const quiz = document.getElementById('quiz');
 const questionEl = document.getElementById('question');
 const a_text = document.getElementById('a_text');
 const b_text = document.getElementById('b_text');
@@ -48,10 +50,12 @@ const d_text = document.getElementById('d_text');
 const submitBtn = document.getElementById('submit');
 
 let currentQuiz = 0;
+let score = 0;
 
 loadQuiz();
 
 function loadQuiz(){
+    deselectAnswers()
 const currentQuizData = quizData[currentQuiz];
 
 questionEl.innerText = currentQuizData.question;
@@ -61,15 +65,42 @@ b_text.innerText = currentQuizData.b;
 c_text.innerText = currentQuizData.c;
 d_text.innerText = currentQuizData.d;
 
-
-
 }
-submitBtn.addEventListener("click" , () => {
-    currentQuiz++;
-    if(currentQuiz < quizData.length){
-       
-        loadQuiz();
-    }else{
-    alert("You finished your quiz");
+function getSelected(){
+   
+    let answer = undefined;
+
+    answerEls.forEach((answerEl) => {
+        if(answerEl.checked){
+            answer = answerEl.id
+        }
+    });
+    return answer;
+    
+}
+function deselectAnswers() {
+    answerEls.forEach((answerEl)=>{
+        answerEl.checked = false;
+        });
     }
-});
+
+submitBtn.addEventListener("click" , () => {
+   
+    const answer = getSelected();
+    console.log(answer);
+    if(answer){
+        if(answer === quizData[currentQuiz].correct)
+        {
+            score++;
+        }
+        currentQuiz++;
+        if(currentQuiz < quizData.length){
+            
+            loadQuiz();
+        }else{
+        quiz.innerHTML = `<h2>You answered correctly ${score}/${quizData.length} Questions.</h2>
+        <button onclick="location.reload()">Reload<button>`;
+        }
+    }
+    });
+    
